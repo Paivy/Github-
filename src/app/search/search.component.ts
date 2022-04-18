d37079aa6eb254821a7c51ad7c2348d6e868cf2c
Paivy <paivyeshirera@gmail.com>
2022-04-18 22:17:13 +0300

    add get function

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from '../services/user.service';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
+  userInput: string = '';
+  user: any;
+  constructor(private userservice: UserService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  getUsername(userName: string) {
+    console.log(userName);
+    this.userservice.getUser(userName).then((result) => this.user=result);
   }
-
 }
