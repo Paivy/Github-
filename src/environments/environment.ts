9d3b2dd4591978ccdc43643bdb53dfcc9acafaa7
Paivy <paivyeshirera@gmail.com>
2022-04-18 22:16:20 +0300

    add github link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  githubapiurl: 'https://api.github.com',
 };
 
 /*
