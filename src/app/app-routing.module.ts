eee97773013ec91be377ac61888dd807a084a5e6
Paivy <paivyeshirera@gmail.com>
2022-04-18 19:02:12 +0300

    add routing modules of serach navbar and home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
+import { LandingComponent } from './landing/landing.component';
+import { FormComponent } from './form/form.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Form', component: FormComponent },
+  { path: 'search', component: FormComponent },
+  { path: 'Landing', component: LandingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
